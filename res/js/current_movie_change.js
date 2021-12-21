0dba748a00f0a5ecc7d83817ab602ea0c83aa2c8
dydtmd6801 <dydtmd6801@naver.com>
2021-12-22 02:17:35 +0900

    popup -> modal, daegu theater loaction

--- res/js/current_movie_change.js
+++ res/js/current_movie_change.js
@@ -2,55 +2,174 @@ var movieData = {
     0 : {
         "subject" : "스파이더맨: 노 웨이 홈",
         "checkPercent" : "84.8",
-        "date" : "2021.12.15"
+        "date" : "2021.12.15",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     1 : {
         "subject" : "엔칸토: 마법의 세계",
         "checkPercent" : "0.5",
-        "date" : "2021.11.24"
+        "date" : "2021.11.24",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     2 : {
         "subject" : "연애 빠진 로맨스",
         "checkPercent" : "0.4",
-        "date" : "2021.11.24"
+        "date" : "2021.11.24",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     3 : {
         "subject" : "극장판 소드 아트 온라인 -프로그레시브-  별 없는 밤의 아리아",
         "checkPercent" : "0.3",
-        "date" : "2021.12.09"
+        "date" : "2021.12.09",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     4 : {
         "subject" : "아멜리에",
         "checkPercent" : "0.3",
-        "date" : "2021.12.15"
+        "date" : "2021.12.15",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     5 : {
         "subject" : "몬스타엑스 : 더 드리밍",
         "checkPercent" : "0.3",
-        "date" : "2021.12.08"
+        "date" : "2021.12.08",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     6 : {
         "subject" : "돈 룩 업",
         "checkPercent" : "0.2",
-        "date" : "2021.12.08"
+        "date" : "2021.12.08",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     7 : {
         "subject" : "유체이탈자",
         "checkPercent" : "0.2",
-        "date" : "2021.11.24"
+        "date" : "2021.11.24",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     8 : {
         "subject" : "프렌치 디스패치",
         "checkPercent" : "0.1",
-        "date" : "2021.11.18"
+        "date" : "2021.11.18",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     },
     9 : {
         "subject" : "피부를 판 남자",
         "checkPercent" : "0.1",
-        "date" : "2021.12.16"
+        "date" : "2021.12.16",
+        "summary" : "‘미스테리오’의 계략으로 세상에 정체가 탄로난\
+            스파이더맨 ‘피터 파커’는 하루 아침에 평범한 일상을 잃게 된다.\
+            문제를 해결하기 위해 ‘닥터 스트레인지’를 찾아가 도움을 청하지만\
+            뜻하지 않게 멀티버스가 열리면서 각기 다른 차원의 불청객들이 나타난다.\
+            ‘닥터 옥토퍼스’를 비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에\
+            ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…",
+        "director" : "존 왓슨",
+        "actor" : "톰 홀랜드, 젠데이아 콜먼, 베네딕트 컴버배치",
+        "eng-subject" : "Spider-Man: No Way Home",
+        "movie-type" : "액션, 모험, SF",
+        "running-time" : "148",
+        "country" : "미국"
     }
 }
-console.log(movieData[0]["subject"]);
 
 function auto_scroll(){
     var img_array = ["movie1.jpg", "movie2.jpg", "movie3.jpg", "movie4.jpg"];
@@ -82,14 +201,15 @@ function auto_scroll(){
     setInterval(auto_img_scroll, 2000);
     
     for(var i = 0; i < 10; i++){
-        movie_img_num = i + 1;
-        url = "./res/img/main_movie" + movie_img_num + ".jpg";
+        url = "./res/img/main_movie" + (i + 1) + ".jpg";
         document.getElementsByClassName("content")[0].innerHTML += 
         "<article class='total_zone'>" +
             "<div class='img_zone' onmouseover='show_hover_menu("+ i +")' onmouseout='hide_hover_menu("+ i +")'>"   + 
                 "<img src='"+ url +"' class='img_sty'>" +
                 "<div class='hover_menu'>"  +
-                    "<button class='btn_sty'>상세보기</button>"  +
+                    "<button class='btn_sty' onclick='show_modal("+ i +")'>" +
+                    "상세보기" +
+                    "</button>"  +
                 "</div>" +
             "</div>" +
             "<div class='data_zone'>"   +
@@ -101,74 +221,44 @@ function auto_scroll(){
     }
 }
 
-function show_hover_menu(check_poster){
-    switch(check_poster){
-        case 0:
-            document.getElementsByClassName("hover_menu")[0].style.visibility = "visible";
-            break;
-        case 1:
-            document.getElementsByClassName("hover_menu")[1].style.visibility = "visible";
-            break;
-        case 2:
-            document.getElementsByClassName("hover_menu")[2].style.visibility = "visible";
-            break;
-        case 3:
-            document.getElementsByClassName("hover_menu")[3].style.visibility = "visible";
-            break;
-        case 4:
-            document.getElementsByClassName("hover_menu")[4].style.visibility = "visible";
-            break;
-        case 5:
-            document.getElementsByClassName("hover_menu")[5].style.visibility = "visible";
-            break;
-        case 6:
-            document.getElementsByClassName("hover_menu")[6].style.visibility = "visible";
-            break;
-        case 7:
-            document.getElementsByClassName("hover_menu")[7].style.visibility = "visible";
-            break;
-        case 8:
-            document.getElementsByClassName("hover_menu")[8].style.visibility = "visible";
-            break;
-        case 9:
-            document.getElementsByClassName("hover_menu")[9].tsyle.visibility = "visible";
-            break;
+function show_modal(check_poster){
+    document.getElementsByClassName("modal-content")[0].innerHTML = "\
+    <article class='modal_article'>\
+        <div class='modal_img_zone'>\
+            <img src='./res/img/main_movie"+ (check_poster + 1) + ".jpg' style='width: 184px; height: 262px;'>" +
+        "</div>\
+        <div class='modal_movie_data'>\
+            <p>" + movieData[check_poster]["subject"] + "</p>\
+            <p>" + movieData[check_poster]["eng-subject"] + "</p>\
+            <p>장르 :" + movieData[check_poster]["movie-type"] + "</p>\
+            <p>감독 : " + movieData[check_poster]["director"] + "</p>\
+            <p>배우 : " + movieData[check_poster]["actor"] + "</p>\
+            <p>개봉 나라 : " + movieData[check_poster]["country"] + "</p>\
+            <p>상영 시간 : " + movieData[check_poster]["running-time"] + "분</p>\
+            <p>개봉 날짜 : " + movieData[check_poster]["date"] + "</p>\
+        </div>\
+        <div class='modal_summary'>\
+            <p>" + movieData[check_poster]["summary"] + "</p>\
+        </div>\
+        <button onclick='close_modal()'>확인</button>\
+    </article>";
+    document.getElementsByClassName("modal")[0].style.visibility = "visible";
+}
+function close_modal() {
+    document.getElementsByClassName("modal")[0].style.visibility = "hidden";
+}
+window.onclick = function(event) {
+    if (event.target == document.getElementsByClassName("modal")[0]) {
+        document.getElementsByClassName("modal")[0].style.visibility = "hidden";
     }
 }
 
+function show_hover_menu(check_poster){
+    document.getElementsByClassName("hover_menu")[check_poster].style.visibility = "visible";
+}
+
 function hide_hover_menu(check_poster){
-    switch(check_poster){
-        case 0:
-            document.getElementsByClassName("hover_menu")[0].style.visibility = "hidden";
-            break;
-        case 1:
-            document.getElementsByClassName("hover_menu")[1].style.visibility = "hidden";
-            break;
-        case 2:
-            document.getElementsByClassName("hover_menu")[2].style.visibility = "hidden";
-            break;
-        case 3:
-            document.getElementsByClassName("hover_menu")[3].style.visibility = "hidden";
-            break;
-        case 4:
-            document.getElementsByClassName("hover_menu")[4].style.visibility = "hidden";
-            break;
-        case 5:
-            document.getElementsByClassName("hover_menu")[5].style.visibility = "hidden";
-            break;
-        case 6:
-            document.getElementsByClassName("hover_menu")[6].style.visibility = "hidden";
-            break;
-        case 7:
-            document.getElementsByClassName("hover_menu")[7].style.visibility = "hidden";
-            break;
-        case 8:
-            document.getElementsByClassName("hover_menu")[8].style.visibility = "hidden";
-            break;
-        case 9:
-            document.getElementsByClassName("hover_menu")[9].tsyle.visibility = "hidden";
-            break;
-    }
+    document.getElementsByClassName("hover_menu")[check_poster].style.visibility = "hidden";
 }
 
 function change_movie_img(img_num){
